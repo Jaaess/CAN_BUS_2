0bc5ca78bbe5197a08142236b64457f01116c8d4
JawharMbarek <mbarek.jawhar@yahoo.de>
2018-06-11 13:30:14 +0200

    added 3 id Filters with reclear

--- src/peak/can/SimpleTest.java
+++ src/peak/can/SimpleTest.java
@@ -32,7 +32,7 @@ public class SimpleTest {
         // JNI Initialization
         pcanBasic.initializeAPI();
 
-        TPCANHandle pcan_handle = TPCANHandle.PCAN_PCIBUS1;
+        TPCANHandle pcan_handle = TPCANHandle.PCAN_PCIBUS2;
         TPCANType pcan_type = TPCANType.PCAN_TYPE_NONE;
         TPCANBaudrate pcan_baudrate = TPCANBaudrate.PCAN_BAUD_100K;
 
@@ -40,38 +40,75 @@ public class SimpleTest {
 
         try {
             res = pcanBasic.Initialize(pcan_handle, TPCANBaudrate.PCAN_BAUD_100K, TPCANType.PCAN_TYPE_NONE, 0, (short) 0);
+//            res = pcanBasic.FilterMessages(pcan_handle, 0x000, 0x7ff, TPCANMode.PCAN_MODE_STANDARD);
+//            res = pcanBasic.FilterMessages(pcan_handle, 0x00000000, 0x7fffffff, TPCANMode.PCAN_MODE_STANDARD);
 
-            res = pcanBasic.FilterMessages(pcan_handle, 0x000, 0x700, TPCANMode.PCAN_MODE_STANDARD);
             if (res == TPCANStatus.PCAN_ERROR_OK) {
-//                {
-//                    TPCANMsg msg = new TPCANMsg();
-//                    msg.setID(0x700);
-//                    msg.setData(new byte[]{0x02, 0x3E, 0x00}, (byte) 3);
-//                    msg.setType(TPCANMessageType.PCAN_MESSAGE_STANDARD);
-//                    res = pcanBasic.Write(pcan_handle, msg);
-//                    System.out.println("Write: " + res);
-//                }
 
-                for (int i = 0; i < 1000; i++) {
-                    {
-                        TPCANMsg msg = new TPCANMsg();
-                        TPCANTimestamp timestamp = new TPCANTimestamp();
-                        msg = new TPCANMsg();
-                        res = pcanBasic.Read(pcan_handle, msg, timestamp);
-                        System.out.println("Read: " + res);
-                        System.out.println(convertToString(msg));
-                        try {
-                            Thread.sleep(100);
-                        } catch (InterruptedException e) {
-                        }
-                    }
+                try {
+                    Thread.sleep(20000);
+                } catch (InterruptedException e) {
                 }
+
+                TPCANMsg msg = new TPCANMsg();
+                TPCANTimestamp timestamp = new TPCANTimestamp();
+//                res = pcanBasic.FilterMessages(pcan_handle, 0x3C3, 0x3C3, TPCANMode.PCAN_MODE_STANDARD);
+                res = pcanBasic.FilterMessages(pcan_handle, 0x001, 0x003, TPCANMode.PCAN_MODE_STANDARD);
+                res = pcanBasic.Read(pcan_handle, msg, timestamp);
+                System.out.println("Read: " + res);
+                System.out.println(convertToString(msg));
+
+//                byte[] bytes = new byte[14];
+//                ByteBuffer buffer = ByteBuffer.allocateDirect(50);
+                TPCANParameterValue iBuffer = TPCANParameterValue.PCAN_FILTER_CLOSE;
+
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                }
+
+                // close the filter
+                pcanBasic.SetValue(pcan_handle, TPCANParameter.PCAN_MESSAGE_FILTER, iBuffer, iBuffer.toString().length());
+                // Set the filter again
+                res = pcanBasic.FilterMessages(pcan_handle, 0x400, 0x43F, TPCANMode.PCAN_MODE_STANDARD);
+                TPCANMsg msg2 = new TPCANMsg();
+                res = pcanBasic.Read(pcan_handle, msg2, timestamp);
+                System.out.println("Read: " + res);
+                System.out.println(convertToString(msg2));
+
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                }
+
+                // close the filter
+                pcanBasic.SetValue(pcan_handle, TPCANParameter.PCAN_MESSAGE_FILTER, iBuffer, iBuffer.toString().length());
+                // Set the filter again
+                res = pcanBasic.FilterMessages(pcan_handle, 0x555, 0x555, TPCANMode.PCAN_MODE_STANDARD);
+                TPCANMsg msg3 = new TPCANMsg();
+                res = pcanBasic.Read(pcan_handle, msg3, timestamp);
+                System.out.println("Read: " + res);
+                System.out.println(convertToString(msg3));
+
+//                for (int i = 0; i < 1000; i++) {
+//                    TPCANMsg msg;
+//                    TPCANTimestamp timestamp = new TPCANTimestamp();
+//                    msg = new TPCANMsg();
+//                    res = pcanBasic.Read(pcan_handle, msg, timestamp);
+//                    System.out.println("Read: " + res);
+//                    System.out.println(convertToString(msg));
+//                    try {
+//                        Thread.sleep(100);
+//                    } catch (InterruptedException e) {
+//                    }
+//                }
 
             } else {
                 System.out.println("Error by reading the data");
-                new RuntimeException("Error by reading the data");
+//                new RuntimeException("Error by reading the data");
             }
         } finally {
+            pcanBasic.Reset(pcan_handle);
             res = pcanBasic.Uninitialize(pcan_handle);
         }
     }
